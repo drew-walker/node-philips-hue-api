ee562c77de303747c87304de6859126132e9f286
Drew Walker <drew.walker@gmail.com>
2015-03-11 23:55:34 +1100

    Added ability to authenticate using public Hue API.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = require('./index_module.js')(
     require('request'),
+    require('url'),
     require('q')
 );
--- index_module.js
+++ index_module.js
@@ -1,11 +1,10 @@
-module.exports = function (request, q) {
-    return function(apiUrl) {
-        var self = {};
+module.exports = function (request, url, q) {
+    return function(username, email, password) {
+        var hue = {};
 
-        self.apiUrl = apiUrl;
-
-        self.lights = function lights(lightIdentifier) {
+        hue.lights = function lights(lightIdentifier) {
             var self = {};
+            var apiUrl = 'http://' + hue.bridge.internalipaddress + '/api/' + username + '/';
 
             self.deferred = q.defer();
 
@@ -82,22 +81,131 @@ module.exports = function (request, q) {
                 }
             };
 
-            self.state = function(state) {
-                if (self.deferred.promise.inspect().state === 'pending') {
-                    self.deferred.promise.then(self.state.bind(self, state));
-                } else {
-                    request.put({
-                        url: apiUrl + 'lights/' + self.lightIdentifier + '/state',
-                        form: JSON.stringify(state)
-                    });
+            function sendMessage(state) {
+                var clipmessage = {
+                    bridgeId : hue.bridge.id,
+                    clipCommand : {
+                        url: "/api/" + username + "/lights/" + self.lightIdentifier + "/state",
+                        method: "PUT",
+                        body: state
+                    }
+                };
 
-                    return self;
+                request.post("https://www.meethue.com/api/sendmessage?token=" + hue.token, {
+                    form: {
+                        clipmessage : JSON.stringify(clipmessage)
+                    }
+                });
+            }
+
+            self.state = function(state) {
+                if (hue.token) {
+                    sendMessage(state);
+                } else {
+                    if (self.deferred.promise.inspect().state === 'pending') {
+                        self.deferred.promise.then(self.state.bind(self, state));
+                    } else {
+                        request.put({
+                            url: apiUrl + 'lights/' + self.lightIdentifier + '/state',
+                            form: JSON.stringify(state)
+                        });
+
+                        return self;
+                    }
                 }
             };
 
             return self;
         };
 
-        return self;
+        function login(options) {
+            var deferred = q.defer();
+
+            request({
+                url:"https://www.meethue.com/en-us/api/getaccesstokengivepermission",
+                method:"POST",
+                jar : options.cookies,
+                form: {
+                    email: options.email,
+                    password: options.password
+                }
+            }, function() {
+                deferred.resolve(options.cookies);
+            });
+
+            return deferred.promise;
+        }
+
+        function getToken(cookies) {
+            var deferred = q.defer();
+
+            request({
+                "url" : "https://www.meethue.com/en-us/api/getaccesstokenpost",
+                "followRedirect" : false,
+                "jar" : cookies
+            }, function(error, response) {
+                deferred.resolve(
+                    url.parse(response.headers.location, true)
+                        .query.redirectUrl.replace('phhueapp://sdk/login/', ''));
+            });
+
+            return deferred.promise;
+        }
+
+        function getBridge() {
+            var deferred = q.defer();
+
+            request({
+                url: "https://www.meethue.com/api/nupnp",
+                json: true
+            }, function(error, response, body) {
+                deferred.resolve(body[0]);
+            });
+
+            return deferred.promise;
+        }
+
+        function getAuthenticationDetails(bridgeId) {
+            var deferred = q.defer(),
+                cookies = request.jar();
+
+            request({
+                url : "https://www.meethue.com/en-us/api/gettoken?appid=hueapp&devicename=iPhone+5&deviceid=" + bridgeId,
+                jar : cookies
+            }, function() {
+                deferred.resolve({ email:email, password:password, cookies:cookies });
+            });
+
+            return deferred.promise;
+        }
+
+        hue.authenticate = function() {
+            var deferred = q.defer();
+
+            function returnOrGetToken(bridge) {
+                hue.bridge = bridge;
+                if (hue.token) {
+                    deferred.resolve(hue.lights);
+                } else {
+                    getAuthenticationDetails(bridge.id)
+                        .then(login)
+                        .then(getToken)
+                        .then(function(token) {
+                            hue.token = token;
+                            deferred.resolve(hue.lights);
+                        });
+                }
+            }
+
+            if (hue.bridge && hue.token) {
+                deferred.resolve(hue.lights);
+            } else {
+                getBridge().then(returnOrGetToken);
+            }
+
+            return deferred.promise;
+        };
+
+        return hue;
     };
 };
